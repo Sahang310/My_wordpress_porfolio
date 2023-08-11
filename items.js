f02b115b6aadf3ff2a00d4e840352d686139713b
sahang310 <sahang310@gmail.com>
2023-08-11 11:53:03 +0530

    My Portfolio web

---
+++ items.js
@@ -0,0 +1,31 @@
+
+
+const posts = []
+
+const images = [
+    'https://images.unsplash.com/photo-1691468800130-060a134a58d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
+    'https://plus.unsplash.com/premium_photo-1673706007413-9f6ede022596?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1691468800130-060a134a58d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1691525737402-e7a447ec2be5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
+    'https://images.unsplash.com/photo-1690975243919-4aa73dc78c81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1691099654855-240bcc540374?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1691428156167-167877254e16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1684254149400-22d7eaec54ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1691334010872-9c9a2a92c56e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+]
+
+let imageIndex = 0;
+
+for(let i = 1; i <= 80; i++){
+    let item = {
+        id: i,
+        title: `Post ${i}`,
+        date: `${i < 10 ? 0 : ''}${i}/10/2021 `,
+        image: images[imageIndex]
+    }
+    posts.push(item);
+    imageIndex++;
+    if (imageIndex > images.length - 1) imageIndex = 0;
+}
+
+console.log(posts)
